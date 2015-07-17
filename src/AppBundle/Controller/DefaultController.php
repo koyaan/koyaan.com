abf28e0f0de7e6396f2126630a9a615150655088
Armin Weihbold <armin.weihbold@pulpmedia.at>
2015-07-17 14:47:01 +0200

    switch to sqlite

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,4 +15,12 @@ class DefaultController extends Controller
     {
         return $this->render('default/index.html.twig');
     }
+
+	/**
+	 * @Route("/app/empty", name="empty")
+	 */
+	public function emptyAction()
+	{
+		return new Response("",Response::HTTP_NO_CONTENT);
+	}
 }
